4adc4db397fb7aadb19c026fbbe3c006feb4a6f9
sam2611 <sakshimishra2611@gmail.com>
2023-09-12 19:06:50 +0530

    Refs continued

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
 
 
 function App() {
@@ -57,7 +58,7 @@ function App() {
      {/* <ParentComp/> */}
      {/* <RefsDemo/> */}
      {/* <FocusInput/> */}
-     
+     <FRParentInput/>
     </div> 
   );
 }
---
+++ src/components/FRParentInput.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import FRInput from './FRInput'
+
+class FRParentInput extends Component {
+    constructor(props) {
+      super(props)
+    this.inputRef= React.createRef()
+    }
+    
+  render() {
+    return (
+      <div>
+        <FRInput ref={this.inputRef}/>
+        <button>Focus input</button>
+      </div>
+    )
+  }
+}
+
+export default FRParentInput
