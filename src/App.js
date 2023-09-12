a228fe6df50a0def7bcdc6e94f85fc223a641ed1
sam2611 <sakshimishra2611@gmail.com>
2023-09-12 18:06:45 +0530

    Refs continued

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import Table from './components/Table';
 import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
+import FocusInput from './components/FocusInput';
 
 
 function App() {
@@ -54,7 +55,9 @@ function App() {
      {/* <FragmentDemo/> */}
      {/* <Table/> */}
      {/* <ParentComp/> */}
-     <RefsDemo/>
+     {/* <RefsDemo/> */}
+     {/* <FocusInput/> */}
+     
     </div> 
   );
 }
