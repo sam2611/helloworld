89841a087cebcb4a63f703b019d154e897ea202f
sam2611 <sakshimishra2611@gmail.com>
2023-09-18 18:39:53 +0530

    error boundary

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
 import FRParentInput from './components/FRParentInput';
 import PortalDemo from './components/PortalDemo';
+import Hero from './components/Hero';
+import ErrorBoundary from './components/ErrorBoundary';
 
 
 function App() {
@@ -60,7 +62,19 @@ function App() {
      {/* <RefsDemo/> */}
      {/* <FocusInput/> */}
      {/* <FRParentInput/> */}
-     <PortalDemo/>
+     {/* <PortalDemo/>  */}
+      <ErrorBoundary>  {/*catch the error and display the fall back ui */}
+     <Hero heroName="Batman"/>
+     </ErrorBoundary>
+
+     <ErrorBoundary>
+     <Hero heroName="Superman"/>
+     </ErrorBoundary>
+     
+     <ErrorBoundary>
+     <Hero heroName="Joker"/>
+     </ErrorBoundary>
+
     </div> 
   );
 }
