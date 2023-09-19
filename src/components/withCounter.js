5fd4b474ad1e4418389b40891fb9cb9a65f57414
sam2611 <sakshimishra2611@gmail.com>
2023-09-20 00:00:54 +0530

    HOC-II

---
+++ src/components/withCounter.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const withCounter = WrappedComponenet=>{
+    class WithCounter extends React.Component{
+        constructor(props) {
+            super(props)
+          
+            this.state = {
+               count: 0
+            }
+          }
+        
+          incrementCount=()=>{
+            this.setState(prevState=>{
+              return {count : prevState.count+1 }
+            })
+          }
+
+        render(){
+            return <WrappedComponenet count={this.state.count} incrementCount={this.incrementCount}/>
+        }
+    }
+    return WithCounter
+}
+
+export default withCounter
